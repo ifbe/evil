902f6d6f0954f54cdef3b6846e17e01b739df820
ifbe <ifbe@live.cn>
2017-10-20 21:50:46 +0800

    nomore malloc 256blocks(1m/block) at begining

--- 1.funland/strhash.c
+++ 1.funland/strhash.c
@@ -28,6 +28,30 @@ struct hash
 	u64 first;
 	u64 last;
 };
+struct tree
+{
+	//hash
+	u64 begin;
+	u64 end;
+
+	//length
+	u64 cur;
+	u64 len;
+
+	//child
+	u64 left;
+	u64 right;
+
+	//memory
+	u64 type;
+	union{
+		void* buf;
+		u64 pad;
+	};
+};
+static struct tree btnode[0x100];
+static int btlen = 0;
+//
 #define maxlen 0x100000
 static u8* hashbuf[256];
 static int hashfd[256];
@@ -56,6 +80,85 @@ u32 djb2hash(char* buf, int len)
 	}
 	return hash;
 }
+
+
+
+
+void* tree_split(struct tree* orig)
+{
+	int j;
+	int begin;
+	u8* src;
+	u8* dst;
+	void* mem;
+	struct tree* this;
+
+	//check
+	if(btlen >= 0x100)
+	{
+		printf("error@btlen\n");
+		return 0;
+	}
+
+	//alloc
+	mem = malloc(0x100000);
+	if(mem == 0)
+	{
+		printf("error@malloc\n");
+		return 0;
+	}
+
+	//move node
+	dst = (void*)btnode + (btlen+1)*sizeof(struct tree) - 1;
+	src = dst - sizeof(struct tree);
+	this = (void*)orig + sizeof(struct tree);
+	btlen++;
+	while(1)
+	{
+		*dst = *src;
+
+		dst--;
+		src--;
+		if(src < (u8*)this)break;
+	}
+
+	//write
+	this->len = 0x100000;
+	this->buf = mem;
+
+	//move hash
+	dst = mem;
+	src = orig->buf;
+	begin = ((orig->cur)/2)&0xffffffffffffff20;
+	for(j=0;j<begin;j++)dst[j] = src[begin+j];
+
+	//change
+	this->cur = (orig->cur) - begin;
+	this->begin = *(u64*)dst;
+	this->end = orig->end;
+	//printf("%016llx,%016llx	%llx\n", this->begin, this->end, this->cur);
+
+	orig->cur = begin;
+	orig->end = this->begin - 1;
+	//printf("%016llx,%016llx	%llx\n", orig->begin, orig->end, orig->cur);
+}
+void* tree_search(u64 hash)
+{
+	int j;
+	for(j=0;j<btlen;j++)
+	{
+		if( (hash >= btnode[j].begin) && (hash <= btnode[j].end) )
+		{
+			return &btnode[j];
+		}
+	}
+	printf("impossiable\n");
+	return 0;
+}
+
+
+
+
 u64 strhash_generate(char* buf, int len)
 {
 	int j;
@@ -77,29 +180,24 @@ u64 strhash_generate(char* buf, int len)
 
 	return *(u64*)this;
 }
-
-
-
-
-void* strhash_search(u64 hash)
+void* strhash_search(void* buf, int len, u64 hash)
 {
 	u64 xxx;
-	int index, mid, left, right;
-	index = (hash >> 56);// & 0xf0;
+	int mid, left, right;
 
 	left = 0;
-	right = hashlen[index]/0x20;
+	right = len/0x20;
 	while(1)
 	{
 		//lastone
 		if(left >= right)
 		{
-			return hashbuf[index] + (left*0x20);
+			return buf + (left*0x20);
 		}
 
 		//
 		mid = (left+right)/2;
-		xxx = *(u64*)(hashbuf[index] + (mid*0x20));
+		xxx = *(u64*)(buf + (mid*0x20));
 		if(xxx < hash)
 		{
 			left = mid+1;
@@ -110,13 +208,18 @@ void* strhash_search(u64 hash)
 		}
 		else
 		{
-			return hashbuf[index] + (mid*0x20);
+			return buf + (mid*0x20);
 		}
 	}
 }
 void strhash_print(u64 hash)
 {
-	struct hash* h = strhash_search(hash);
+	struct tree* t;
+	struct hash* h;
+	t = tree_search(hash);
+	if(t == 0)return;
+
+	h = strhash_search(t->buf, t->cur, hash);
 	if(h == 0)return;
 	if(*(u64*)h != hash)return;
 
@@ -133,40 +236,35 @@ void strhash_print(u64 hash)
 
 
 
-void stoplearn();
 void* strhash_write(char* buf, int len)
 {
 	int j;
-	int index;
-	struct hash* h;
+	u64 temp;
 	u8* src;
 	u8* dst;
-	u64 temp;
-
-	//
+	struct tree* t;
+	struct hash* h;
 	temp = strhash_generate(buf, len);
 
-	//notfound
-	h = strhash_search(temp);
-	if(h == 0)return 0;
-
-	//repeat
-	if(*(u64*)h == temp)return h;
-
-	//
-	index = (temp >> 56);// & 0xf0;
-	hashlen[index] += 0x20;
-	if(	((index == 0) && (hashlen[index] >= 0x1000000 - 0x20))
-	|	((index != 0) && (hashlen[index] >= maxlen - 0x20)) )
+	//which
+	t = tree_search(temp);
+	if(t == 0)return 0;
+	if((t->cur) >= (t->len))
 	{
-		printf("err@strhash");
-		stoplearn();
-		return 0;
+		printf("tree split: %llx,%llx\n", t->cur, t->len);
+		tree_split(t);
+		t = tree_search(temp);
 	}
 
-	//move
-	dst = hashbuf[index] + hashlen[index] + 0x1f;
+	//where
+	h = strhash_search(t->buf, t->cur, temp);
+	if(h == 0)return 0;
+	if(*(u64*)h == temp)return h;
+
+	//move 
+	dst = (t->buf) + (t->cur) + 0x1f;
 	src = dst - 0x20;
+	t->cur += 0x20;
 	while(1)
 	{
 		*dst = *src;
@@ -193,7 +291,12 @@ void* strhash_write(char* buf, int len)
 }
 void* strhash_read(u64 hash)
 {
-	struct hash* h = strhash_search(hash);
+	struct tree* t;
+	struct hash* h;
+	t = tree_search(hash);
+	if(t == 0)return 0;
+
+	h = strhash_search(t->buf, t->cur, hash);
 	if(h == 0)return 0;
 	if(*(u64*)h != hash)return 0;
 	return h;
@@ -206,54 +309,139 @@ void strhash_choose()
 }
 void strhash_start(int type)
 {
-	int j, k;
-	int flag1, flag2;
+	int j, fd;
 	char name[32];
-	u8* p;
-	if(type != 0)printf("strhash:\n");
-
-	for(j=0;j<256;j++)
+	void* mem;
+	struct tree* t;
+	if(type == 0)
 	{
-		//name
-		snprintf(name, 32, ".42/strhash/%02x", j);
-		flag1 = O_CREAT|O_RDWR|O_BINARY;
-		if(type == 0)flag1 |= O_TRUNC;
-		flag2 = S_IRWXU|S_IRWXG|S_IRWXO;
-
-		//open
-		hashfd[j] = open(name, flag1, flag2);
-		if(hashfd[j] <= 0)
+		//alloc
+		mem = malloc(0x100000);
+		if(mem == 0)
 		{
-			printf("error@hashfd%02x\n", j);
-			exit(-1);
+			printf("error@malloc: %s\n", name);
+			return;
+		}
+		t = btnode;
+		btlen = 1;
+
+		t->begin = 0;
+		t->end = 0xffffffffffffffff;
+		t->cur = 0;
+		t->len = 0x100000;
+		t->buf = mem;
+		return;
+	}
+
+	//open index
+	snprintf(name, 32, ".42/strhash/index");
+	fd = open(name,
+		O_CREAT|O_RDWR|O_BINARY,
+		S_IRWXU|S_IRWXG|S_IRWXO
+	);
+
+	//read index
+	j = read(fd, btnode, sizeof(btnode));
+	btlen = j / sizeof(struct tree);
+	printf("strhash:	%x\n", btlen);
+
+	//close index
+	close(fd);
+
+	//
+	for(j=0;j<btlen;j++)
+	{
+		//open
+		snprintf(name, 32, ".42/strhash/%02x", j);
+		fd = open(name,
+			O_CREAT|O_RDWR|O_BINARY,
+			S_IRWXU|S_IRWXG|S_IRWXO
+		);
+		if(fd <= 0)
+		{
+			printf("error@open: %s\n", name);
+			break;
 		}
 
-		//malloc
-		if(j == 0)p = malloc(0x1000000);
-		else p = malloc(maxlen);
-		hashbuf[j] = p;
+		//alloc
+		mem = malloc(0x100000);
+		if(mem == 0)
+		{
+			printf("error@malloc: %s\n", name);
+			break;
+		}
+		t = &btnode[j];
+		t->len = 0x100000;
+		t->buf = mem;
 
 		//read
-		if(type == 0)k = 0;
-		else
+		t->cur = read(fd, t->buf, t->len);
+		if(t->cur == 0)
 		{
-			k = read(hashfd[j], p, maxlen);
-			//printf("	%02x:	%x\n", j, k);
+			printf("error@read: %s\n", name);
+			break;
 		}
-		hashlen[j] = k;
 
-		//clean
-		for(;k<maxlen;k++)p[k] = 0;
+		//close
+		close(fd);
 	}
 }
 void strhash_stop()
 {
 	int j;
-	for(j=0;j<256;j++)
+	int fd;
+	int ret;
+	char name[32];
+	struct tree* t;
+
+	//open index
+	snprintf(name, 32, ".42/strhash/index");
+	fd = open(name,
+		O_CREAT|O_RDWR|O_TRUNC|O_BINARY,
+		S_IRWXU|S_IRWXG|S_IRWXO
+	);
+	if(fd <= 0)
 	{
-		lseek(hashfd[j], 0, SEEK_SET);
-		write(hashfd[j], hashbuf[j], hashlen[j]);
-		close(hashfd[j]);
+		printf("error@open: %s\n", name);
+		return;
+	}
+
+	//write index
+	ret = write(fd, btnode, btlen*sizeof(struct tree));
+	if(ret < 0)
+	{
+		printf("error@write: %s\n", name);
+		return;
+	}
+
+	//close index
+	close(fd);
+
+	for(j=0;j<btlen;j++)
+	{
+		//open data
+		snprintf(name, 32, ".42/strhash/%02x", j);
+		fd = open(name,
+			O_CREAT|O_RDWR|O_TRUNC|O_BINARY,
+			S_IRWXU|S_IRWXG|S_IRWXO
+		);
+		if(fd <= 0)
+		{
+			printf("error@open: %s\n", name);
+			break;
+		}
+
+		//write data
+		t = &btnode[j];
+		ret = write(fd, t->buf, t->cur);
+		if(ret < 0)
+		{
+			printf("error@write: %s\n", name);
+			break;
+		}
+
+		//close data
+		close(fd);
 	}
 }
 void strhash_create()
--- 2.nadir/string/name.c
+++ 2.nadir/string/name.c
@@ -13,33 +13,31 @@ void name_write(char* buf, int len)
 	}
 	worker_write(buf, len, 4, 0);
 
-	j=0;
-	k=0;
+	j = 0;
+	k = -1;
 	while(1)
 	{
-		val = buf[j];
+		if(j >= len)val = 0;
+		else val = buf[j];
+
 		if( ( (val >= '0')&&(val <= '9') ) |
 			( (val >= 'A')&&(val <= 'Z') ) |
 			( (val >= 'a')&&(val <= 'z') ) |
-			(val == '.') ){flag = 1;}
-		else{flag = 0;}
-
-		if((flag == 1)&&(k == 0))k = j;
-		if(k != 0)
+			(val == '.'))
 		{
-			if(j+1 == len)
-			{
-				flag = 0;
-				j++;
-			}
-			if(flag == 0)
-			{
-				worker_write(buf+k, j-k, 3, 0);
-				k = 0;
-			}
+			if(k<0)k = j;
+			flag = 0;
+		}
+		else flag = 1;
+
+		if( (flag == 1) && (j-k < len) )
+		{
+			worker_write(buf+k, j-k, 3, 0);
+			//printf("%.*s\n", j-k, buf+k);
+			k = -1;
 		}
 
 		j++;
-		if(j >= len)break;
+		if(j > len)break;
 	}
 }
