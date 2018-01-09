05c96a7f575ca0c107cfce789620059ed43dca4f
ifbe <ifbe@live.cn>
2018-01-09 22:09:33 +0800

    asm conv: ac2att, ac2intel

--- 0.learn/conv.c
+++ 0.learn/conv.c
@@ -54,9 +54,163 @@ int intel2ac(int argc, char** argv)
 
 
 
+void ac2att_fmt(char* dst, int brack, char* src, int len)
+{
+	int j,k,t;
+	int bl,il,ol;
+	char* base=0;
+	char* index=0;
+	char* scale="1";
+	char* offset=0;
+
+	if(brack == 0)
+	{
+		if((src[0] >= '0')&&(src[0] <= '9'))dst[0] = '$';
+		else dst[0] = '%';
+		snprintf(dst+1, 32, "%.*s", len, src);
+	}
+	else
+	{
+		//printf("<%.*s>",len,src);
+		k = 0;
+		for(j=0;j<=len;j++)
+		{
+			if((j==len)|(src[j] == '+')|(src[j] == '-'))
+			{
+				if((src[k]>='a')&&(src[k]<='z'))
+				{
+					base = src+k;
+					bl = j-k;
+				}
+				else if((src[k]=='-')|((src[k]>='0')&&(src[k]<='9')))
+				{
+					offset = src+k;
+					ol = j-k;
+				}
+
+				if(src[j] == '-')k = j;
+				else k = j+1;
+			}
+			else if(src[j] == '*')
+			{
+				for(t=j+1;t<=len;t++)
+				{
+					if((src[t] == '+')|(src[t] == '-'))break;
+				}
+
+				if((src[k]>='a')&&(src[k]<='z'))
+				{
+					index = src+k;
+					il = j-k;
+					scale = src+j+1;
+				}
+				else if((src[k]>='0')&&(src[k]<='9'))
+				{
+					scale = src+k;
+					index = src+j+1;
+					il = t-j-1;
+				}
+
+				j = t;
+				if(src[t] == '-')k = t;
+				else k=t+1;
+			}
+		}
+
+		j=0;
+		if(offset != 0)j = snprintf(dst,32,"%.*s",ol,offset);
+		if((base==0)&&(index==0))return;
+
+		j += snprintf(dst+j, 32-j, "(");
+		if(base != 0)
+		{
+			j += snprintf(dst+j, 32-j,
+				"%%%.*s", bl, base
+			);
+		}
+		if(index != 0)
+		{
+			j += snprintf(dst+j, 32-j,
+				",%%%.*s,%.*s",
+				il, index,
+				1, scale
+			);
+		}
+		j += snprintf(dst+j, 32-j, ")");
+	}
+	//printf("{%s}",dst);
+}
+void ac2att_line(char* buf, int len)
+{
+	int j,k;
+	char* opcode = 0;
+	char fmt1[32];
+	char fmt2[32];
+	for(j=0;j<len;j++)
+	{
+		if(	(buf[j] == '=') | (buf[j] == '?') |
+			(buf[j] == '<') | (buf[j] == '>') |
+			(buf[j] == '+') | (buf[j] == '-') |
+			(buf[j] == '*') )
+		{
+			break;
+		}
+	}
+	if(j >= len)
+	{
+		printf("%.*s\n",len,buf);
+		return;
+	}
+
+	if(buf[j] == '=')
+	{
+		for(k=j+1;k<len;k++)
+		{
+			if(	(buf[k] == '[') |
+				(buf[k] == '*') |
+				(buf[k] == '+') |
+				(buf[k] == '-') )
+			{break;}
+		}
+		if(k >= len)
+		{
+			opcode = "mov";
+			ac2att_fmt(fmt1, 0, buf+j+1, len-j-1);
+		}
+		else
+		{
+			if(buf[k] == '[')
+			{
+				opcode = "mov";
+				ac2att_fmt(fmt1, 1, buf+k+1, len-k-2);
+			}
+			else
+			{
+				opcode = "lea";
+				ac2att_fmt(fmt1, 1, buf+j+1, len-j-1);
+			}
+		}
+		ac2att_fmt(fmt2, 0, buf, j);
+	}
+	else
+	{
+		if(buf[j] == '?')opcode = "cmp";
+		else if(buf[j] == '<')opcode = "shl";
+		else if(buf[j] == '>')opcode = "shr";
+		else if(buf[j] == '+')opcode = "add";
+		else if(buf[j] == '-')opcode = "sub";
+		else if(buf[j] == '*')opcode = "imul";
+
+		ac2att_fmt(fmt1, 0, buf+j+1, len-j-1);
+		ac2att_fmt(fmt2, 0, buf, j);
+	}
+
+	printf("%s ", opcode);
+	printf("%s,%s\n", fmt1, fmt2);
+}
 int ac2att(int argc, char** argv)
 {
-	int j,fd,len;
+	int j,k,fd,len;
 	char* buf = 0;
 
 	fd = open(argv[1], O_RDONLY|O_BINARY);
@@ -69,6 +223,16 @@ int ac2att(int argc, char** argv)
 	if(len <= 0)return -3;
 
 	close(fd);
+
+	k = 0;
+	for(j=0;j<=len;j++)
+	{
+		if((j==len) | (buf[j]=='\n'))
+		{
+			ac2att_line(buf+k, j-k);
+			k=j+1;
+		}
+	}
 }
 
 
@@ -76,51 +240,107 @@ int ac2att(int argc, char** argv)
 
 void ac2intel_line(char* buf, int len)
 {
-	int j;
+	int j,k;
 	for(j=0;j<len;j++)
 	{
-		if(buf[j] == '=')
+		if(	(buf[j] == '=') | (buf[j] == '?') |
+			(buf[j] == '<') | (buf[j] == '>') |
+			(buf[j] == '+') | (buf[j] == '-') |
+			(buf[j] == '*') )
 		{
-			printf("mov ");
-			buf[j] = ',';
-		}
-		else if(buf[j] == '?')
-		{
-			printf("cmp ");
-			buf[j] = ',';
-		}
-		else if(buf[j] == '<')
-		{
-			printf("shl ");
-			buf[j] = ',';
-		}
-		else if(buf[j] == '>')
-		{
-			printf("shr ");
-			buf[j] = ',';
-		}
-		else if(buf[j] == '+')
-		{
-			printf("add ");
-			buf[j] = ',';
-		}
-		else if(buf[j] == '-')
-		{
-			printf("sub ");
-			buf[j] = ',';
-		}
-		else if(buf[j] == '*')
-		{
-			printf("imul ");
-			buf[j] = ',';
-		}
-		else if(buf[j] == '/')
-		{
-			printf("idiv ");
-			buf[j] = ',';
+			break;
 		}
 	}
-	printf("%.*s\n",len,buf);
+	if(j >= len)
+	{
+		printf("%.*s\n",len,buf);
+	}
+	else if(buf[j] == '=')
+	{
+		for(k=j+1;k<len;k++)
+		{
+			if(	(buf[k] == '[') |
+				(buf[k] == '*') |
+				(buf[k] == '+') |
+				(buf[k] == '-') )
+			{break;}
+		}
+		if((k >= len)|(buf[k] == '['))
+		{
+			printf("mov %.*s,%.*s\n",
+				j,buf,
+				len-j-1,buf+j+1
+			);
+		}
+		else
+		{
+			printf("lea %.*s,[%.*s]\n",
+				j,buf,
+				len-j-1,buf+j+1
+			);
+		}
+	}
+	else if(buf[j] == '?')
+	{
+		printf("cmp %.*s,%.*s\n",
+			j,buf,
+			len-j-1,buf+j+1
+		);
+	}
+	else if(buf[j] == '<')
+	{
+		printf("shl %.*s,%.*s\n",
+			j,buf,
+			len-j-1,buf+j+1
+		);
+	}
+	else if(buf[j] == '>')
+	{
+		printf("shr %.*s,%.*s\n",
+			j,buf,
+			len-j-1,buf+j+1
+		);
+	}
+	else if(buf[j] == '+')
+	{
+		printf("add %.*s,%.*s\n",
+			j,buf,
+			len-j-1,buf+j+1
+		);
+	}
+	else if(buf[j] == '-')
+	{
+		printf("sub %.*s,%.*s\n",
+			j,buf,
+			len-j-1,buf+j+1
+		);
+	}
+	else if(buf[j] == '*')
+	{
+		for(k=j+1;k<len;k++)
+		{
+			if(buf[k] == '*')break;
+		}
+		if(k < len)
+		{
+			printf("imul %.*s,%.*s,%.*s\n",
+				j,buf,
+				k-j-1,buf+j+1,
+				len-k-1,buf+k+1
+			);
+		}
+		else
+		{
+			printf("imul %.*s,%.*s\n",
+				j,buf,
+				len-j-1,buf+j+1
+			);
+		}
+	}
+	else if(buf[j] == '/')
+	{
+		printf("idiv %.*s,%.*s\n",j,buf,len-j-1,buf+j+1);
+	}
 }
 int ac2intel(int argc, char** argv)
 {
