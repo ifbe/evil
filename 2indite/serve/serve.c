4e664fd8c550dc1328878279f463f23f5efdbcab
ifbe <ifbe@live.cn>
2018-02-04 12:42:32 +0800

    fix

--- 2indite/serve/serve.c
+++ 2indite/serve/serve.c
@@ -18,64 +18,65 @@ int search_one(void*, int, void*, int);
 static u8 response[] =
 	"HTTP/1.1 200 OK\r\n"
 	"\r\n";
+static u8 htmlpath[0x200];
+static int htmlroot = 0;
+static u8 codepath[0x200];
+static int coderoot = 0;
 
 
 
 
-int servesocket_url(char* wbuf, int wlen, char* buf, int len)
+int servesocket_html(char* wbuf, int wlen, char* buf, int len)
 {
-	int j,fd,ret = 0;
-	for(j=0;j<len;j++)
-	{
-		if(buf[j] <= 0x20)
-		{
-			buf[j] = 0;
-			ret = j;
-			break;
-		}
-	}
+	int fd,ret = 0;
+	char path[0x1000];
 
-	if(ret == 0)
-	{
-		fd = open("datafile/index.html", O_RDONLY);
-		if(fd <= 0)return 0;
-	}
-	else
-	{
-		fd = open(buf, O_RDONLY);
-		if(fd <= 0)return 0;
-	}
+	ret = snprintf(path, 0x1000, "%.*s", htmlroot, htmlpath);
+	if(len == 0)snprintf(path+ret, 0x1000, "index.html");
+	else snprintf(path+ret, 0x1000, "%.*s", len, buf);
 
-	j = snprintf(wbuf, 0x80, "%s", response);
-	ret = read(fd, wbuf+j, wlen-j);
+	fd = open(path, O_RDONLY);
+	if(fd <= 0)return 0;
+
+	ret = read(fd, wbuf, wlen);
 	if(ret <= 0)
 	{
 		close(fd);
 		return 0;
 	}
-	j += ret;
 
 	close(fd);
-	return j;
+	return ret;
+}
+int servesocket_code(char* wbuf, int wlen, char* buf, int len)
+{
+	int fd,ret = 0;
+	char path[0x1000];
+	if(len == 0)return 0;
+
+	ret = snprintf(path, 0x1000, "%.*s", coderoot, codepath);
+	snprintf(path+ret, 0x1000, "%.*s", len, buf);
+
+	fd = open(path, O_RDONLY);
+	if(fd <= 0)return 0;
+
+	ret = read(fd, wbuf, wlen);
+	if(ret <= 0)
+	{
+		close(fd);
+		return 0;
+	}
+
+	close(fd);
+	return ret;
 }
 int servesocket_search(char* wbuf, int wlen, char* buf, int len)
 {
-	int j,ret = 0;
-	for(j=0;j<0x1000;j++)
-	{
-		if(buf[j] <= 0x20)
-		{
-			buf[j] = 0;
-			ret = j;
-			break;
-		}
-	}
-	j = snprintf(wbuf, 0x80, "%s", response);
-	j += search_one(wbuf+j, wlen-j, buf, ret);
-	return j;
+	return search_one(wbuf, wlen, buf, len);
 }
 int servesocket(char* wbuf, int wlen, char* buf, int len)
 {
+	int j,k,ret;
 	printf("%.*s", len, buf);
 	if(strncmp(buf, "GET /", 5) != 0)
 	{
@@ -83,20 +84,35 @@ int servesocket(char* wbuf, int wlen, char* buf, int len)
 		return 0;
 	}
 
-	if(buf[5] != '?')
+	for(j=5;j<len;j++)
 	{
-		return servesocket_url(wbuf, wlen, buf+5, len-5);
+		if(buf[j] <= 0x20)break;
+	}
+
+	k = snprintf(wbuf, 0x80, "%s", response);
+	if(buf[5] == '?')
+	{
+		ret = servesocket_search(wbuf+k, wlen, buf+6, j-6);
+	}
+	else if(buf[5] == '!')
+	{
+		ret = servesocket_code(wbuf+k, wlen, buf+6, j-6);
 	}
 	else
 	{
-		return servesocket_search(wbuf, wlen, buf+6, len-6);
+		ret = servesocket_html(wbuf+k, wlen, buf+5, j-5);
 	}
+
+	if(ret <= 0)return 0;
+	else return k+ret;
 }
 void serve(int argc, char** argv)
 {
 	int port;
 	if(argc == 1)port = 80;
 	else sscanf(argv[1], "%d", &port);
+	htmlroot = snprintf(htmlpath, 0x200, "datafile/");
+	coderoot = 0;
 
 	readthemall(1);
 	startsocket(port);
